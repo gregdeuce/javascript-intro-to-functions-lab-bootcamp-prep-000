2dc332b57697bb70fcfb0616c2f12aeee7fe335d
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-12 18:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase())
+  return "YES INDEED!"
+}
