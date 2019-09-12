c2498634841ceb977a1f4d7adbe1cd2b664aff1c
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-12 18:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function whisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
-  console.log("I can't hear you!")
+  return "I can't hear you!"
 }
 
 function logShout(string) {
