1f09dfc8d3da4b007de27c174698c641d79dd78a
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-12 18:22:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,8 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase())
   return "YES INDEED!"
 }
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+  return "I love you, too."
+}
